f14e228b9628e5e8c5d2d96a94c21e6f61d1c979
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-30 00:36:51 +0530

    feat: add databaseURL
    feat: add GET and POST methods
    
    feat: implement body-parser

--- index.js
+++ index.js
@@ -2,16 +2,61 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-var serviceAccount = require("./serviceAccount.json"); //add downloadedd service account JSON file path
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+//add downloadedd service account JSON file path
+var serviceAccount = require("./serviceAccount.json"); 
 
 initializeApp({
-    credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
+  databaseURL: "peapleapi.firebaseio.com"
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/projects", (req, res) => {
+  try {
+    const db = getFirestore();
+    const people = db.collection("projects");
+    people.get().then((snapshot) => {
+      const projects = [];
+      snapshot.forEach((doc) => {
+        projects.push(doc.data());
+      });
+      res.send(projects);
+    });
+  } catch (error) {
+    res.status(500);
+    res.send({
+      message: "Error getting projects",
+      error: error,
+    });
+  }
+});
+
+app.post("/projects", (req, res) => {
+  const requestBody = req.body;
+  try {
+    const db = getFirestore();
+    const project = db.collection("projects");
+    project.add(requestBody).then(() => {
+      res.send({
+        message: "Project added successfully",
+        project: requestBody,
+      });
+    });
+  } catch (error) {
+    res.status(500);
+    res.send({
+      message: "Error adding project",
+      error: error,
+    });
+  }
 });
 
 app.listen(port, () => {

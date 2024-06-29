15b4742e70717e74bc4d7ceaf2d57aa5b3023812
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-29 23:38:57 +0530

    feat: add firebase SDK

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { initializeApp, cert } = require('firebase-admin/app');
+var serviceAccount = require("serviceAccount.json"); //add downloadedd service account JSON file path
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });

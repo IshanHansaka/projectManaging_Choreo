2cd11c5af435306201e55bca48eb182b03670ff6
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-06-30 10:03:43 +0530

    feat: implement cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const cors = require('cors');
+app.use(cors());
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
